f4dd4403e8d1db7d5311e634f198712a103a62f5
sancar bal <sncr.bl@hotmail.com>
2024-02-22 17:18:08 +0300

    feat: swagger ui enabled for prod as well

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -83,15 +83,9 @@ builder.Services.AddSwaggerGen(opt =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-else
-{
-    app.UseExceptionMiddleware();
-}
+app.UseExceptionMiddleware();
+app.UseSwagger();
+app.UseSwaggerUI();
 
 using var scope = app.Services.CreateScope();
 
